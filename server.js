5e013d02f0330cda8ffee84a0eba4b2a0d39d5fe
HampusAyalla <hampus.melinosterlindh@skola.taby.se>
2026-04-20 21:15:50 +0200

    La till kolla token o börja på att skap a användare

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Importerar moduler som mysql
 var mysql = require('mysql');
 var express = require('express');
 var jwt = require('jsonwebtoken');
@@ -7,10 +8,12 @@ var app = express();
 var http = require('http').Server(app);
 var port = 5000;
 
-app.use(express.json());
+app.use(express.json()); //Läsa Json request 
 
+//Hemlig nyckel som man måste ha
 const SECRET = "supersecretkey";
 
+//Koppling till min databas (Som jag inte har än)
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -18,7 +21,41 @@ var con = mysql.createConnection({
   database: "databasnamnet"
 });
 
+//Kopplar till databasen
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Kopplad till databas. ");
+  console.log("Kopplad till databas");
 });
+
+//middleware, kollar om användaren har en token för att kolla om man ska kunna gå vidare
+function auth(req, res, next) {
+  const token = req.headers['mintoken']; //
+
+  if (!token) {
+    return res.status(401).json({ message: "Ingen token" });
+  }
+
+  jwt.verify(token, SECRET, function(err, decoded) {
+    if (err) {
+      return res.status(403).json({ message: "Ogiltig token" });
+    }
+
+    req.user = decoded; //Spara användaren 
+    next();
+  });
+}
+
+
+// Skapar ny användare
+app.post("/users", async function(req, res) {
+  const { username, password } = req.body;
+
+  // Kolla så du skrivit namn o lösen
+  if (!username || !password) {
+    return res.status(400).json({ message: "Fyll i alla fält" });
+  }
+
+  // Hashar lösenordet
+  const hash = await bcrypt.hash(password, 10);
+
+};
